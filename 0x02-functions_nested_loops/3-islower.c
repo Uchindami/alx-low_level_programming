8957ffa77f7ae9dba982abeb34b28d1a86069173
Uchindami <manfredchirambojz@gmail.com>
2023-03-22 15:13:30 -0700

    im cheating guys

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,6 +5,9 @@
 	 * _islower - check if char is lowercase
 	 * @c: is the char to be checked
 	 *
+	 * Description: This function checks if a given char is a lowercase letter.
+	 * If it is, it returns 1. Otherwise, it returns 0.
+	 *
 	 * Return: 1 if char is lowercase, otherwise 0.
 	 */
 
