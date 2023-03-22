3905730396cbad0bb8dfcb2fd0eee82fbce23d14
Uchindami <manfredchirambojz@gmail.com>
2023-03-21 22:23:01 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,4 +6,9 @@
  * Return: 0 on success
  */
 int main(void)
-{char str[] = "_putchar\n";int i;for (i = 0; str[i] != '\0'; i++)_putchar(str[i]);return (0);}
+{
+char str[] = "_putchar\n";
+int i;
+for (i = 0; str[i] != '\0'; i++)_putchar(str[i]);
+return (0);
+}
