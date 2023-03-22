19c2f1f91decbc2d5a21fa0908ce14757db98e98
Uchindami <manfredchirambojz@gmail.com>
2023-03-22 15:21:14 -0700

    im cheating guys

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,22 @@
+File Edit Options Buffers Tools C Help                                                                                                                                                                         
 #include "main.h"
 
 
-	/**
-	 * _islower - check if char is lowercase
-	 * @c: is the char to be checked
-	 *
-	 * Description: This function checks if a given char is a lowercase letter.
-	 * If it is, it returns 1. Otherwise, it returns 0.
-	 *
-	 * Return: 1 if char is lowercase, otherwise 0.
-	 */
+        /**                                                                                                                                                                                                    
+         * _islower - check if char is lowercase                                                                                                                                                               
+         * @c: is the char to be checked                                                                                                                                                                       
+         *                                                                                                                                                                                                     
+         * Description: This function checks if a given char is a lowercase letter.                                                                                                                            
+         * If it is, it returns 1. Otherwise, it returns 0.                                                                                                                                                    
+         *                                                                                                                                                                                                     
+         * Return: 1 if char is lowercase, otherwise 0.                                                                                                                                                        
+         */
 
 
-	int _islower(int c)
-	{
-		if (c >= 'a' && c <= 'z')
-			return (1);
-		else
-			return (0);
-	}
+        int _islower(int c)
+        {
+                if (c >= 'a' && c <= 'z')
+                        return (1);
+                else
+                        return (0);
+        }
