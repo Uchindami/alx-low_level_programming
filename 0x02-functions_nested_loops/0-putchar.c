f5acfbdf7b282dc1ad0ae03dec43f62856b1bf3d
Uchindami <manfredchirambojz@gmail.com>
2023-03-21 22:32:13 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * main - prints "_putchar" followed by a new line
  *
@@ -7,9 +8,11 @@
  */
 int main(void)
 {
-char str[] = "_putchar\n";
-int i;
-for (i = 0; str[i] != '\0'; i++)
-  _putchar(str[i]);
-return (0);
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (0);
 }
