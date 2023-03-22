47f7a8833c3c7c49ac0abc5c481c2083dc8965bd
Uchindami <manfredchirambojz@gmail.com>
2023-03-21 22:11:02 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <unistd.h>
 
+/**
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+    char str[] = "_putchar\n";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+        _putchar(str[i]);
 
     return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
 int _putchar(char c);
 
 #endif /* MAIN_H */
+
