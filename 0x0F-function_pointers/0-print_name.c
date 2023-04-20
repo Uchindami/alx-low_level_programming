139d7e8df2744e1e8baaf8a5cbdaad1910199f99
Uchindami <manfredchirambojz@gmail.com>
2023-04-19 17:57:25 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
- **/
+ * print_name - Prints a name using a function pointer
+ * @name: Pointer to a character array containing the name
+ * @f: Pointer to a function that takes a character pointer as an argument
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    // Check if name and f are not NULL
-    if (name != NULL && f != NULL)
-    {
-        // Call the function pointed to by f with name as argument
-        f(name);
-    }
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
