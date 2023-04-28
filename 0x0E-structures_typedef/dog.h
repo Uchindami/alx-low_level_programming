836c3d27379dbd3fb56c6190db22a10305190e56
Uchindami <manfredchirambojz@gmail.com>
2023-04-28 01:55:48 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,74 +2,26 @@
 #define DOG_H
 
 /**
- * @struct dog
- * @brief A dog's basic info
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * @var name The dog's name
- * @var age The dog's age
- * @var owner The dog's owner
+ * Description: 
  */
-struct dog {
-  char *name;
-  float age;
-  char *owner;
-};
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
 
-/**
- * @typedef dog_t
- * @brief A typedef for struct dog
- */
-typedef struct dog dog_t;
 
-/**
- * @brief Initialize a dog
- *
- * @param d A pointer to the dog to initi-
- * @param name The dog's name
- * @param age The dog's age
- * @param owner The dog's owner
- */
-void init_dog(dog_t *d, const char *name, float age, const char *owner);
-
-/**
- * @brief Print a dog
- *
- * @param d A pointer to the dog to print
- */
+void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(const dog_t *d);
-
-/**
- * @brief Create a new dog
- *
- * @param name The dog's name
- * @param age The dog's age
- * @param owner The dog's owner
- * @return A pointer to the newly created dog, or NULL on error
- */
 dog_t *new_dog(const char *name, float age, const char *owner);
-
-/**
- * @brief Free a dog
- *
- * @param d A pointer to the dog to free
- */
 void free_dog(dog_t *d);
-
-/**
- * @brief Copy a str
- *
- * @param dest The dest str
- * @param src The source str
- * @return A pointer to the dest str, or NULL on error
- */
 char *_strcpy(char *dest, const char *src);
-
-/**
- * @brief Get the length of a str
- *
- * @param s The str
- * @return The length of the str
- */
 int _strlen(const char *s);
 
 #endif
