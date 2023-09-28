a8c2e2deb5945ee344ceac7b188c5ca2315c0feb
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-09-28 22:47:56 +0200

    fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,7 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		entry_node->prev = NULL;
 		*head = entry_node;
-		return entry_node;
+		return (entry_node);
 	}
 
 	while (holding->next)
